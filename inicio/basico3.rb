838f3137da10de836f2c65b8550517660ee747fa
joohnyxxz <jessica.maldonado339@gmail.com>
2026-02-02 10:57:03 -0300

    Operadores básicos

---
+++ inicio/basico3.rb
@@ -0,0 +1,19 @@
+# Operadores pt1
+# Divisão: /
+# Multiplicação: *
+# Soma: +
+# Subtração: -
+# Resto da Divião: %
+# Exponencial: **
+
+# Exemplo:
+# Usaremos o número 17
+num = 17
+
+puts num / 2    # 8
+puts num / 2.0  # 8.5
+puts num * 2    # 34
+puts num + 5    # 22
+puts num - 2    # 15
+puts num % 3    # 2
+puts num ** 2   # 289
