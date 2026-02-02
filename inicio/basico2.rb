0402856f6d3c84b4b318a9f39063b0207a08321e
joohnyxxz <jessica.maldonado339@gmail.com>
2026-02-02 10:50:11 -0300

    Tipos de valores

---
+++ inicio/basico2.rb
@@ -0,0 +1,21 @@
+# Tipos de valores:
+# Float - 7.5 - Tipo: Float
+# Integer - 7 - Tipo: Integer
+# Boolean - true/false - Tipo: TrueClass/FalseClass
+# String - "Palavra" - Tipo: String
+
+# Para verificar o tipo da variável utilizamos ".class"
+num = 7
+puts num.class
+
+num2 = 7.5
+puts num2.class
+
+bool = true
+puts bool.class
+
+bool2 = false
+puts bool2.class
+
+palavra = "Hola"
+puts palavra.class
