57dd29e0c4dbe29e2446e878fa23e78e422dc6bc
joohnyxxz <jessica.maldonado339@gmail.com>
2026-02-02 10:43:11 -0300

    Printando mensagem e atribuindo valor à variável

---
+++ inicio/basico.rb
@@ -0,0 +1,7 @@
+# Utilizamos "puts" para printar uma mensagem
+# Aspas duplas e aspas simples funcionam igual
+puts "Hello World"
+
+# Variáveis são iguais python
+mensagem = "Olá Mundo"
+puts mensagem
